266474f47f02233dfdbfbb751388623102ea4380
Jonathan Maw <jonathan.maw@gmail.com>
2021-06-11 17:58:51 +0100

    Initial commit of widgets toolbox

---
+++ main.lua
@@ -0,0 +1,13 @@
+-- vim: set et hlsearch ts=4 sts=4 sw=4:
+
+local uiElements = {}
+
+function love.load()
+    love.window.setTitle("Widget Toolbox")
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
